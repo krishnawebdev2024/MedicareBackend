62c3054a1d86dbd392bcb05545211514e3e1ff62
krishna <krisch.webdev@gmail.com>
2024-12-04 09:48:30 +0100

    addedlogincode

--- Controllers/usersControllers.js
+++ Controllers/usersControllers.js
@@ -160,6 +160,10 @@ export const loginUser = async (req, res, next) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      path: "/",
+
       maxAge: 24 * 60 * 60 * 1000,
     });
 
